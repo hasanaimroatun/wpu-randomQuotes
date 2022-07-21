6c66d876a74a4cab985ae7e67335f3eca7608796
hasanaimroatun <imroatunhasana@gmail.com>
2022-07-21 17:41:26 +0700

    membuat file dan cssnya

---
+++ src/components/CreatedBy.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './myStyle.css'
+
+function CreatedBy() {
+  return (
+    <div className='created'>created by <a href="https://github.com/hasanaimroatun/wpu-randomQuotes" className='name'>hasanaimroatun</a> - devChallenges.io </div>
+  )
+}
+
+export default CreatedBy
